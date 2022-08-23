e5b29f2c94e765f029bd0acded93218e421b0361
Dinaka Nwamu <dinakanwamu@gmail.com>
2022-08-23 12:56:28 +0100

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import Card from "./components/Card";
 import ImageSearch from "./components/ImageSearch";
-import React,{useState,useEffect} from "react";
-import { data } from "autoprefixer";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [images, setImages] = useState([]);
-  const[isLoading, setIsLoading] = useState(true);
-  const [term,setTerm] = useState('');
-   useEffect(()=>{
-    fetch(`https://pixabay.com/api/?key=29432420-8b2d522df225578a4f1695b1a&q=${term}&image_type=photo`)
-      .then(res => res.json())
-      .then(data => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=29432420-8b2d522df225578a4f1695b1a&q=${term}&image_type=photo`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         setImages(data.hits);
         setIsLoading(false);
       })
-      .catch(err => console.log(err));
-  },[term]);
-  return(
-    
-      <div className="container mx-auto">
-        <ImageSearch searchText = {(text)=> setTerm(text)}/>
-        {!isLoading && images.length===0 && <h1 className="text-5xl text-center mx-auto mt-32">No images found</h1>}
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1>:<div className="grid grid-cols-3 gap-4">
-        {images.map(image=>(
-          <Card key={image.id} image ={image}/>
-        )) } 
-      </div>}
-      </div>
-    
-    
-  )
+      .catch((err) => console.log(err));
+  }, [term]);
+  return (
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">No images found</h1>
+      )}
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1>
+      ) : (
+        <div className="grid justify-center gap-y-3.5 md:grid-cols-3 md:gap-4">
+          {images.map((image) => (
+            <Card key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,33 @@
-function Card({image}) {
-  const tags = image.tags.split(',');
+function Card({ image }) {
+  const tags = image.tags.split(",");
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img
-        src={image.webformatURL}
-        className="w-full"
-        alt="Random Image"
-      ></img>
+      <img src={image.webformatURL} className="w-full" alt="Random Image"></img>
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl">
           Photo by {image.user}
         </div>
         <ul>
           <li>
-              <strong>Views:{image.views}</strong>
+            <strong>Views:{image.views}</strong>
           </li>
           <li>
-              <strong>Downloads:{image.downloads}</strong>
+            <strong>Downloads:{image.downloads}</strong>
           </li>
           <li>
-              <strong>Likes:{image.likes}</strong>
+            <strong>Likes:{image.likes}</strong>
           </li>
         </ul>
       </div>
       <div className="px-6 py-4">
-        {tags.map( (tag,index)=>(
-          <span key={index} className="inline-block my-1 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block my-1 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+          >
+            #{tag}
+          </span>
         ))}
-       
       </div>
     </div>
   );
